801ab4cfea5b8d37fbbe6e30638dd086500e304b
AmoCat <jqzhu@ir.hit.edu.cn>
2016-10-02 23:55:27 +0800

    .

--- bit_operation/405. Convert a Number to Hexadecimal.cpp
+++ bit_operation/405. Convert a Number to Hexadecimal.cpp
@@ -1,18 +1,14 @@
+const string HEX="0123456789abcdef";
 class Solution {
 public:
     string toHex(int num) {
         if(num==0) return "0";
-        int x = 0x000f;
+        int x = 0xf;
         string res="";
         while(num!=0){
-            res = hash(num&x)+res;
+            res = HEX(num&x)+res;
             num=(num>>4)&0x0fffffff;
         }
         return res;
-    }
-private:
-    char hash(int x){
-        if(x<10) {return '0'+x;}
-        else return 'a'+x-10;
     }
 };
